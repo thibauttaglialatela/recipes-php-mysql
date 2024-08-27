9de8550271d186041454c800ab395fce72e45bf5
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-08-27 15:43:15 +0200

    feat: add the delete method

--- delete_recipe.php
+++ delete_recipe.php
@@ -1,5 +1,8 @@
 <?php
+include_once __DIR__ . "/connect.php";
 if (isset($_POST['recipe_id']) && !empty($_POST['recipe_id'])) {
-printf('La recette %s est supprimée', htmlspecialchars($_POST['recipe_id']));
-//todo: implémenter la suppression de la recette
+$sql = "DELETE FROM `recipes` WHERE `recipe_id` = :recipe_id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['recipe_id' => $_POST['recipe_id']]);
+header('Location: index.php');
 }
